3431bcb4e3285325913942fa80724bd7241598f3
Guilherme Gomes <guilhermeneto26@hotmail.com>
2023-12-23 13:05:00 -0300

    Add optional parameter to torrar function

--- JavaScript/7. Funções/func.js
+++ JavaScript/7. Funções/func.js
@@ -1,6 +1,7 @@
 //funçoes com parametro
-function torrar(pao){
+function torrar(pao, nome = "Cliente"){ //sempre deixar a variavel opcional por ultimo
     console.log("torrada feita com "+pao)
+    console.log("ele é um pedido de "+nome)
 }
 
-torrar("pão de forma")
+torrar("pão de forma","Anitta")
