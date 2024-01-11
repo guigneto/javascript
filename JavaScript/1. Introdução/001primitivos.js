44b5a0ceb12b7c1501f6a296d944b7e6af3473f8
Guilherme Gomes <guilhermeneto26@hotmail.com>
2024-01-10 22:07:24 -0300

    Add JavaScript file with primitive variable examples

---
+++ JavaScript/1. Introdução/001primitivos.js
@@ -0,0 +1,9 @@
+let nome ="Gemo"; // String
+let idade = 20; // Number
+let altura = 1.65; // Number
+let temFaculdade = true; // Boolean
+let ehFeio = false; // Boolean
+let nulo = null; // Null: variavel que não se sabe o valor (ainda)
+let indefinido; // Undefined
+
+console.log(typeof nome, typeof idade, typeof altura, typeof temFaculdade, typeof ehFeio, typeof nulo, typeof indefinido)
